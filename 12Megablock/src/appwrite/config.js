38aee2a0d50412f52c4f6b6755edc9595bce0682
Hamid Khan <hamidkhantanol@gmail.com>
2025-04-28 19:02:52 +0000

    Appwritedatabasefileupload

---
+++ 12Megablock/src/appwrite/config.js
@@ -0,0 +1,10 @@
+import conf from '../conf/conf'
+import {Client, ID, Databases, Storage, Query} from "appwrite"
+
+export class Service{
+    client = n
+}
+
+const service = new Service()
+
+export default service
